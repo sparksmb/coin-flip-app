cea3b32e3e0aa4c666ac9ecb1942b132f4f8fb86
sparksmb <sparksmb@gmail.com>
2014-07-16 10:39:12 -0500

    flipCoin.create() test now passes.

---
+++ coin-flip/flip-coin.js
@@ -0,0 +1,15 @@
+/*global app */
+app.usecase.flipCoin = {
+	create: function () {
+		'use strict';
+		var flipCoin;
+		
+		flipCoin = {
+			execute: function () {
+				
+			}
+		};
+		
+		return flipCoin;
+	}
+};
