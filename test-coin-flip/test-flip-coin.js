a251b7b84a29529dcc6ce392984b92ec8614a868
sparksmb <sparksmb@gmail.com>
2014-07-17 09:26:16 -0500

    completed flip-coin and tests.

--- test-coin-flip/test-flip-coin.js
+++ test-coin-flip/test-flip-coin.js
@@ -3,24 +3,23 @@ module('flip-coin.js');
 
 test('flipCoin.create() - The factory create method should return an object with an execute method.', function () {
 	'use strict';
-	var flipCoin = app.usecase.flipCoin.create();
+	var coin = app.entity.coin.create(),
+		flipCoin = app.usecase.flipCoin.create(coin);
 	strictEqual(typeof flipCoin.execute, 'function', 'create returned an object with an execute method');
 });
 
-//test('flipCoin.execute(0) - Flipping the coin with 0 half rotations should be HEADS. The coin always starts on HEADS before being flipped. Zero indicates the coin was not actually flipped.', function () {
-//	'use strict';
-//	var flipCoin = app.usecase.flipCoin.create();
-//	strictEqual(flipCoin.execute(2), flipCoin.HEADS, 'coin landed on HEADS');
-//});
-//
-//test('flipCoin.execute(1) - Flipping the coin with 1 half rotation should land on TAILS', function () {
-//	'use strict';
-//	var flipCoin = app.usecase.flipCoin.create();
-//	strictEqual(flipCoin.execute(1), flipCoin.TAILS, 'coin landed on tails');
-//});
-//
-//test('flipCoin.execute(2) - Flipping the coin with 2 half rotation should land on HEADS', function () {
-//	'use strict';
-//	var flipCoin = app.usecase.flipCoin.create();
-//	strictEqual(flipCoin.execute(2), flipCoin.HEADS, 'coin landed on HEADS');
-//});
+test('flipCoin.execute(1) - Flipping the coin with 1 half rotation should land on TAILS', function () {
+	'use strict';
+	var coin = app.entity.coin.create(),
+		flipCoin = app.usecase.flipCoin.create(coin);
+	flipCoin.sideUp = flipCoin.execute(1);
+	strictEqual(flipCoin.sideUp, coin.TAILS, 'coin landed on tails');
+});
+
+test('flipCoin.execute(2) - Flipping the coin with 2 half rotation should land on HEADS', function () {
+	'use strict';
+	var coin = app.entity.coin.create(),
+		flipCoin = app.usecase.flipCoin.create(coin);
+	flipCoin.sideUp = flipCoin.execute(2);
+	strictEqual(flipCoin.sideUp, coin.HEADS, 'coin landed on HEADS');
+});
