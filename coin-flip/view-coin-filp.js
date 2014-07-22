ab7a29c229c0c58b3efe5fb7fe1066b27791ef9a
sparksmb <sparksmb@gmail.com>
2014-07-22 18:32:39 -0500

    added view-coin-flip.  test passes.

---
+++ coin-flip/view-coin-filp.js
@@ -0,0 +1,15 @@
+/*global app */
+app.usecase.viewCoinFlip = {
+	create: function (view, callHeadsOrTails, flipCoin) {
+		'use strict';
+		var viewCoinFlip;
+		
+		viewCoinFlip = {
+			execute: function () {
+				throw 'not implemented';
+			}
+		};
+		
+		return viewCoinFlip;
+	}
+};
